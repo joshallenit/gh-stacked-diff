125ee02b7229f75cb4a545e5010fcb23af711d68
Josh Allen <jallen@slack-corp.com>
2022-11-15 12:42:47 -0800

    remove code.sh

--- code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-open /Applications/Visual\ Studio\ Code.app "$@"
--- create-symlinks.sh
+++ create-symlinks.sh
@@ -4,7 +4,6 @@ sudo ln -sfn $PWD/git-updatepr.sh /usr/local/bin/git-updatepr
 sudo ln -sfn $PWD/git-newpr.sh  /usr/local/bin/git-newpr
 sudo ln -sfn $PWD/installapp.sh /usr/local/bin/installapp
 sudo ln -sfn $PWD/gitlog.sh /usr/local/bin/gitlog
-sudo ln -sfn $PWD/code.sh /usr/local/bin/code
 sudo ln -sfn $PWD/git-amendpr.sh /usr/local/bin/git-amendpr
 sudo ln -sfn $PWD/git-reset-main.sh /usr/local/bin/git-reset-main
 sudo ln -sfn $PWD/git-prs.sh /usr/local/bin/git-prs
