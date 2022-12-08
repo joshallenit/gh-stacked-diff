4a0101255aa8e01dbed9bd707e6f63c4208d55e4
Josh Allen <jallen@slack-corp.com>
2022-12-08 17:11:01 -0500

    new-pr draft (#17)

--- go/new-pr.go
+++ go/new-pr.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"os"
 )
 
 func main() {
-	branchInfo := GetBranchInfo(os.Args[1])
+	var draft bool
+	flag.BoolVar(&draft, "draft", true, "Whether to create the PR as draft")
+	flag.Parse()
+
+	branchInfo := GetBranchInfo(flag.Arg(0))
 	prText := GetPullRequestText(branchInfo.CommitHash)
+	log.Println("Switching to branch", branchInfo.BranchName)
 	ExecuteFailable("git", "branch", "--no-track", branchInfo.BranchName, "origin/main")
 	Execute("git", "switch", branchInfo.BranchName)
-	_, cherryPickError := ExecuteFailable("git", "cherry-pick", branchInfo.CommitHash)
+	log.Println("Cherry picking", branchInfo.CommitHash)
+	cherryPickOutput, cherryPickError := ExecuteFailable("git", "cherry-pick", branchInfo.CommitHash)
 	if cherryPickError != nil {
-		log.Println("Could not cherry-pick, aborting...", cherryPickError)
+		log.Println("Could not cherry-pick, aborting...", cherryPickOutput, cherryPickError)
 		Execute("git", "cherry-pick", "--abort")
 		Execute("git", "switch", "main")
 		return
 	}
+	log.Println("Pushing to remote")
 	Execute("git", "-c", "push.default=current", "push", "-f")
-	Execute("gh", "pr", "create" /*"--draft", */, "--title", prText.Title, "--body", prText.Description, "--fill")
+	log.Println("Creating PR via gh")
+	var createPrOutput string
+	if draft {
+		createPrOutput = Execute("gh", "pr", "create", "--draft", "--title", prText.Title, "--body", prText.Description, "--fill")
+	} else {
+		createPrOutput = Execute("gh", "pr", "create", "--title", prText.Title, "--body", prText.Description, "--fill")
+	}
+	log.Println("Created PR", createPrOutput)
 	Execute("gh", "pr", "view", "--web")
+	log.Println("Switching back to main")
 	Execute("git", "switch", "main")
 	/*
 	   This avoids this hint when using `git fetch && git-rebase origin/main` which is not appropriate for stacked diff workflow:
