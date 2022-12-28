4796552fcac2e56f25208bb5b67a5cba4b712e0b
Josh Allen <jallen@slack-corp.com>
2022-12-28 00:30:15 -0500

    new-pr usage

--- cmd/new-pr.go
+++ cmd/new-pr.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
+	var White = "\033[97m"
+	// var Red = "\033[31m"
+	var Reset = "\033[0m"
 	RequireMainBranch()
 	var draft bool
 	var featureFlag string
@@ -13,8 +18,46 @@ func main() {
 	flag.BoolVar(&draft, "draft", true, "Whether to create the PR as draft")
 	flag.StringVar(&featureFlag, "feature-flag", "None", "Value for FEATURE_FLAG in PR description")
 	flag.StringVar(&baseBranch, "base", "main", "Base branch for Pull Request")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr,
+			Reset+"Create a new PR with a cherry-pick of the given commit hash\n"+
+				"\n"+
+				"new-pr [flags] [commit hash to make PR for (default is top commit on main)]\n"+
+				"\n"+
+				White+"Note on Ticket Number:"+Reset+"\n"+
+				"\n"+
+				"If you prefix the Jira ticket to the git commit summary then the `Ticket` section of the PR description will be populated with it.\n"+
+				"For example:\n"+
+				"\"CONV-9999 Add new feature\"\n"+
+				"\n"+
+				White+"Note on Templates:"+Reset+"\n"+
+				"\n"+
+				"The Pull Request Title, Body (aka Description), and Branch Name are created from golang templates. The defaults are:\n"+
+				"\n"+
+				"- branch-name.template - cmd/config/branch-name.template\n"+
+				"- pr-description.template - cmd/config/pr-description.template\n"+
+				"- pr-title.template - cmd/config/pr-title.template\n"+
+				"\n"+
+				"The possible values for the templates are:\n"+
+				"\n"+
+				"- **TicketNumber** - Jira ticket as parsed from the commit summary\n"+
+				"- **Username** -  Name as parsed from git config email\n"+
+				"- **CommitBody** - Body of the commit message\n"+
+				"- **CommitSummary** - Summary line of the commit message\n"+
+				"- **CommitSummaryCleaned** - Summary line of the commit message without spaces or special characters\n"+
+				"- **CommitSummaryWithoutTicket** - Summary line of the commit message without the prefix of the ticket number\n"+
+				"- **FeatureFlag** - Value passed to feature-flag flag\n"+
+				"\n"+
+				"To change a template, copy the default from [cmd/config/](cmd/config/) into `~/.stacked-diff-workflow/` and modify.\n"+
+				"\n"+
+				White+"Flags"+Reset+"\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
-
+	if flag.NArg() > 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
 	branchInfo := GetBranchInfo(flag.Arg(0))
 	prText := GetPullRequestText(branchInfo.CommitHash, featureFlag)
 	var commitToBranchFrom string
