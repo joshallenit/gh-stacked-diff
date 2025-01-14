4d830cacd943fcfb5fece10e3bd35ee9689c987d
Josh Allen <josh.allen.it@gmail.com>
2025-01-14 17:27:22 -0500

    setup test repo

--- src/mains/new-pr/new-pr_test.go
+++ src/mains/new-pr/new-pr_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	sd "stacked-diff-workflow/src/stacked-diff"
 	testing_init "stacked-diff-workflow/src/testing-init"
+	"strings"
 	"testing"
 )
 
@@ -15,14 +16,20 @@ func TestNewPr(t *testing.T) {
 	repositoryDir := "local-repo"
 	//testExecutor := sd.TestExecutor{Dir: testing_init.TestWorkingDir, TestLogger: log.Default()}
 
+	defaultBranch := strings.TrimSpace(sd.ExecuteOrDie(sd.ExecuteOptions{}, "git", "config", "init.defaultBranch"))
 	sd.ExecuteOrDie(sd.ExecuteOptions{}, "git", "init", "--bare", remoteDir)
 	sd.ExecuteOrDie(sd.ExecuteOptions{}, "git", "clone", remoteDir, repositoryDir)
+
 	os.Chdir(repositoryDir)
 	log.Println(sd.ExecuteOrDie(sd.ExecuteOptions{}, "pwd"))
+
+	sd.ExecuteOrDie(sd.ExecuteOptions{}, "touch", "README.md")
+	sd.ExecuteOrDie(sd.ExecuteOptions{}, "git", "add", ".")
+	sd.ExecuteOrDie(sd.ExecuteOptions{}, "git", "commit", "-m", "Add README")
 
 	//sd.SetGlobalExecutor(testExecutor)
 	// Create the branches required and CD into them.
 
-	//CreateNewPr(true, "", "", 0, sd.GetBranchInfo(""), log.Default())
+	CreateNewPr(true, "", "", 0, sd.GetBranchInfo(""), log.Default())
 	t.Fatalf("Test not implemented")
 }
