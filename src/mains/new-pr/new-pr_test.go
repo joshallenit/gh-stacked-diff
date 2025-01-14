4a5ee344fc7e95eb720a0ce40029c913d7d8a315
Josh Allen <josh.allen.it@gmail.com>
2025-01-14 17:16:30 -0500

    First test

--- src/mains/new-pr/new-pr_test.go
+++ src/mains/new-pr/new-pr_test.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 	sd "stacked-diff-workflow/src/stacked-diff"
 	testing_init "stacked-diff-workflow/src/testing-init"
 	"testing"
 )
 
 func TestNewPr(t *testing.T) {
-	testDir := testing_init.RootProjectDir + "/../.test/test-repo"
-	sd.ExecuteOrDie(sd.ExecuteOptions{}, "rm", "-rf", testDir)
-	sd.ExecuteOrDie(sd.ExecuteOptions{}, "mkdir", "-p", testDir)
-	testExecutor := sd.TestExecutor{Dir: testDir, TestLogger: log.Default()}
+	testing_init.CdTestDir()
+	// Create a git repository with a local remote
+	remoteDir := "remote-repo"
+	repositoryDir := "local-repo"
+	//testExecutor := sd.TestExecutor{Dir: testing_init.TestWorkingDir, TestLogger: log.Default()}
 
-	sd.SetGlobalExecutor(testExecutor)
-	// Create the branches required and CD into them.
-
-	// if I need to specify
+	sd.ExecuteOrDie(sd.ExecuteOptions{}, "git", "init", "--bare", remoteDir)
+	sd.ExecuteOrDie(sd.ExecuteOptions{}, "git", "clone", remoteDir, repositoryDir)
+	os.Chdir(repositoryDir)
 	log.Println(sd.ExecuteOrDie(sd.ExecuteOptions{}, "pwd"))
+
+	//sd.SetGlobalExecutor(testExecutor)
+	// Create the branches required and CD into them.
 
 	//CreateNewPr(true, "", "", 0, sd.GetBranchInfo(""), log.Default())
 	t.Fatalf("Test not implemented")
--- src/testing-init/testing-init.go
+++ src/testing-init/testing-init.go
@@ -1,13 +1,21 @@
 package testing_init
 
 import (
+	"os"
 	"path"
 	"runtime"
+	sd "stacked-diff-workflow/src/stacked-diff"
 )
 
-var RootProjectDir string
+var TestWorkingDir string
 
 func init() {
 	_, filename, _, _ := runtime.Caller(0)
-	RootProjectDir = path.Join(path.Dir(filename), "/../..")
+	TestWorkingDir = path.Join(path.Dir(filename), "/../../../.test-stacked-diff-workflow")
+}
+
+func CdTestDir() {
+	sd.ExecuteOrDie(sd.ExecuteOptions{}, "rm", "-rf", TestWorkingDir)
+	sd.ExecuteOrDie(sd.ExecuteOptions{}, "mkdir", "-p", TestWorkingDir)
+	os.Chdir(TestWorkingDir)
 }
