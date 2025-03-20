b8215fafd218779441e01da0933cf6b5323cafcd
Josh Allen <josh.allen.it@gmail.com>
2025-03-20 06:26:51 -0400

    cleanup

--- interactive/table_selection.go
+++ interactive/table_selection.go
@@ -6,6 +6,7 @@ import (
 	bubbletable "github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/joshallenit/gh-stacked-diff/v2/util"
 )
 
 var baseStyle = lipgloss.NewStyle()
@@ -34,7 +35,6 @@ type model struct {
 	windowWidth  int
 	multiselect  bool
 	rowEnabled   func(row int) bool
-	maxRowWidth  int
 	completed    bool
 	prompt       string
 }
@@ -84,52 +84,40 @@ func (m model) View() string {
 	if m.completed {
 		return ""
 	}
-	// oh no I need the resize capabilities
-
-	// columns := util.MapSlice(m.table.Columns(), func(column bubbletable.Column) string {
-	// 	return column.Title
-	// })
-	// rows := util.MapSlice(m.table.Rows(), func(row bubbletable.Row) []string {
-	// 	return row
-	// })
-	// renderTable := lipglosstable.New().Headers(columns...).
-	// 	Rows(rows...).
-	// 	StyleFunc(func(row, col int) lipgloss.Style {
-	// 		switch {
-	// 		case row < 0 || row >= len(rows):
-	// 			// < 0 is the header row
-	// 			// >= len can happen on resize
-	// 			return baseStyle
-	// 		case row == m.table.Cursor():
-	// 			if m.rowEnabled(row) {
-	// 				if slices.Contains(m.selectedRows, row) {
-	// 					return selectedHighlightRowStyle
-	// 				} else {
-	// 					return highlightEnabledStyle
-	// 				}
-	// 			} else {
-	// 				return highlightDisabledStyle
-	// 			}
-	// 		case slices.Contains(m.selectedRows, row):
-	// 			return selectedRowStyle
-	// 		default:
-	// 			if m.rowEnabled(row) {
-	// 				return enabledRowStyle
-	// 			} else {
-	// 				return disabledRowStyle
-	// 			}
-	// 		}
-	// 	}).
-	// 	Width(min(m.maxRowWidth, m.windowWidth-2))
+	m.table.SetStyleFunc(func(tableModel bubbletable.Model, row, col int) lipgloss.Style {
+		switch {
+		case row < 0 || row >= len(tableModel.Rows()):
+			// < 0 is the header row
+			// >= len can happen on resize
+			return baseStyle
+		case row == tableModel.Cursor():
+			if m.rowEnabled(row) {
+				if slices.Contains(m.selectedRows, row) {
+					return selectedHighlightRowStyle
+				} else {
+					return highlightEnabledStyle
+				}
+			} else {
+				return highlightDisabledStyle
+			}
+		case slices.Contains(m.selectedRows, row):
+			return selectedRowStyle
+		default:
+			if m.rowEnabled(row) {
+				return enabledRowStyle
+			} else {
+				return disabledRowStyle
+			}
+		}
+	})
 	return promptStyle.Render(m.prompt) + "\n" + m.table.View() + "\n"
 }
 
 // Returns -1 if the user cancelled.
 func GetTableSelection(prompt string, columns []string, rows [][]string, multiselect bool, rowEnabled func(row int) bool) []int {
-	tableColumns := make([]bubbletable.Column, len(columns))
-	for i, columnName := range columns {
-		tableColumns[i] = bubbletable.Column{Title: columnName, Width: 1}
-	}
+	tableColumns := util.MapSlice(columns, func(columnName string) bubbletable.Column {
+		return bubbletable.Column{Title: columnName}
+	})
 
 	tableRows := make([]bubbletable.Row, len(rows))
 	firstEnabledRow := -1
@@ -138,41 +126,13 @@ func GetTableSelection(prompt string, columns []string, rows [][]string, multise
 		if firstEnabledRow == -1 && rowEnabled(i) {
 			firstEnabledRow = i
 		}
-		for i, cell := range rowData {
-			tableColumns[i].Width = max(tableColumns[i].Width, len(cell))
-		}
 	}
 
 	t := bubbletable.New(
 		bubbletable.WithColumns(tableColumns),
 		bubbletable.WithRows(tableRows),
 		bubbletable.WithFocused(true),
-		bubbletable.WithStyleFunc(func(m bubbletable.Model, row, col int) lipgloss.Style {
-			switch {
-			case row < 0 || row >= len(rows):
-				// < 0 is the header row
-				// >= len can happen on resize
-				return baseStyle
-			case row == m.Cursor():
-				if rowEnabled(row) {
-					// if slices.Contains(m.selectedRows, row) {
-					return selectedHighlightRowStyle
-					// } else {
-					// 	return highlightEnabledStyle
-					// }
-				} else {
-					return highlightDisabledStyle
-				}
-			// case slices.Contains(m.selectedRows, row):
-			// return selectedRowStyle
-			default:
-				if rowEnabled(row) {
-					return enabledRowStyle
-				} else {
-					return disabledRowStyle
-				}
-			}
-		}),
+		bubbletable.WithHeight(8),
 	)
 	if firstEnabledRow != -1 {
 		t.SetCursor(firstEnabledRow)
@@ -183,7 +143,6 @@ func GetTableSelection(prompt string, columns []string, rows [][]string, multise
 		selectedRows: []int{},
 		multiselect:  multiselect,
 		rowEnabled:   rowEnabled,
-		maxRowWidth:  totalWidth(t.Columns()),
 		prompt:       prompt,
 	}
 	finalModel, err := tea.NewProgram(initialModel).Run()
@@ -191,13 +150,4 @@ func GetTableSelection(prompt string, columns []string, rows [][]string, multise
 		panic(err)
 	}
 	return finalModel.(model).selectedRows
-}
-
-func totalWidth(columns []bubbletable.Column) int {
-	totalSize := 0
-	for _, column := range columns {
-		totalSize += column.Width
-	}
-	// Each column has a border, plus 1 for the last border.
-	return totalSize + len(columns) + 1
 }
